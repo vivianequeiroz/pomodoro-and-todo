c18ea1f518c90afb182af4c44cc02ee2d7f65271
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-17 16:27:33 -0300

    chore: general style and initial page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InitialHomeComponent } from './components/initial-home/initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialHomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
