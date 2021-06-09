1c59684751698c85850ebd954368d044da56eefa
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-09 19:27:36 -0300

    refact: stop rest time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PomodoroCountdownComponent } from './components/pomodoro-coutdown/pomodoro-countdown.component';
+import { PomodoroRestComponent } from './components/pomodoro-rest/pomodoro-rest.component';
 import { WorkTimerService } from './services/pomodoro-services/work-timer.service';
 import { RestTimerService } from './services/pomodoro-services/rest-timer.service';
 import { FormatTimePipe } from './pipes/format-time.pipe';
 import { FormsModule } from '@angular/forms';
-
-
 @NgModule({
   declarations: [
     AppComponent,
-    PomodoroCountdownComponent,
+    PomodoroRestComponent,
     FormatTimePipe,
   ],
   imports: [
