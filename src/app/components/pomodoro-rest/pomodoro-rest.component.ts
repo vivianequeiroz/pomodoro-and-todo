c6926fabd13d0bb7440ad0550895eaba542b0dbe
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-13 23:24:34 -0300

    chore: work component

--- src/app/components/pomodoro-rest/pomodoro-rest.component.ts
+++ src/app/components/pomodoro-rest/pomodoro-rest.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { timer, Subscription } from "rxjs";
+// import { Component, OnInit, OnDestroy } from "@angular/core";
+// import { timer, Subscription } from "rxjs";
 
-@Component({
-  selector: "app-pomodoro-rest",
-  templateUrl: "./pomodoro-rest.component.html",
-  styleUrls: ["./pomodoro-rest.component.css"]
-})
-export class PomodoroRestComponent implements OnInit, OnDestroy {
+// @Component({
+//   selector: "app-pomodoro-rest",
+//   templateUrl: "./pomodoro-rest.component.html",
+//   styleUrls: ["./pomodoro-rest.component.css"]
+// })
+// export class PomodoroRestComponent implements OnInit, OnDestroy {
 
-  countDown: Subscription;
+//   countDown: Subscription;
 
-  counter = 30; // in seconds
-  tick = 1000;
+//   counter = 30; // in seconds
+//   tick = 1000;
 
-  ngOnInit() {
-    this.countDown = timer(0, this.tick)
-      .subscribe(() => { 
-      if(this.counter > 0) 
-        --this.counter
-      else 
-        this.ngOnDestroy();
-      });
+//   ngOnInit() {
+//     this.countDown = timer(0, this.tick)
+//       .subscribe(() => { 
+//       if(this.counter > 0) 
+//         --this.counter
+//       else 
+//         this.ngOnDestroy();
+//       });
       
-  }
-  ngOnDestroy(){
-      this.countDown = null;
-  }
-}
+//   }
+//   ngOnDestroy(){
+//       this.countDown = null;
+//   }
+// }
 
