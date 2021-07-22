1777f429e3ccbf3d7c9cbfb7f863bc06251bf5ae
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-07-22 20:30:32 -0300

    refact: componentizing front and back cards

---
+++ src/app/components/timer-card/timer-card.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FrontTimerCardComponent } from '../front-timer-card/front-timer-card.component';
+import { BackTimerCardComponent } from '../back-timer-card/back-timer-card.component';
+
+
+
+@NgModule({
+  declarations: [FrontTimerCardComponent, BackTimerCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TimerCardModule { }
